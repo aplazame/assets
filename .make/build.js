a3aae8c27a2f77a1719582084b0580142dc61ae6
Jesús Germade <jesus@germade.es>
2019-04-11 19:13:26 +0200

    Adding indexes

--- .make/build.js
+++ .make/build.js
@@ -10,11 +10,12 @@ const _sizeOf = require('image-size')
 const sizeOf = promisify(_sizeOf)
 
 const template = require('@triskel/template')
+const marked = require('marked')
 
 template.cmd('cdn', function (expression, scope) {
 
     var file = this.eval(expression)(scope)
-    console.log('url', file)
+    // console.log('url', file)
     return 'https://' + path.join('cdn.aplazame.com/assets', scope.cwd, file.path)
 
 }, true)
@@ -36,19 +37,23 @@ function _whiteTextFile (filepath, data) {
     })
 }
 
-async function writeIndex (cwd) {
+async function writeImagesTable (cwd) {
   var filepaths = await glob('**/*.{png,svg}', {
     cwd: cwd,
-  })
+  }),
+  num_files = 0
   
   var files = await Promise.all( filepaths.map(async function (filepath) {
+    num_files++
     return {
       path: filepath,
       dimensions: await sizeOf( path.join(cwd, filepath) ),
     }
   }) )
 
-  var index_template = await _readTextFile( path.join(__dirname, 'demo/index.html') )
+  console.log(`\nnum_files in '${ cwd }': ${ num_files }\n`)
+
+  var index_template = await _readTextFile( path.join(__dirname, 'demo/images-table.html') )
   
   await _whiteTextFile(
     path.join(cwd, 'index.html'),
@@ -62,7 +67,23 @@ async function writeIndex (cwd) {
   )
 }
 
+async function writeIndex () {
+  var index_template = await _readTextFile( path.join(__dirname, 'demo/images-index.html') )
+  var readme_md = await _readTextFile( path.join(__dirname, '../README.md') )
+  
+  await _whiteTextFile(
+    path.join(__dirname, '../index.html'),
+    template(index_template, {
+      page: {
+        title: 'Aplazame | Assets',
+        body: marked(readme_md),
+      },
+    })
+  )
+}
+
 Promise.all([
-  writeIndex('logos'),
-  glob('banners/*').then( (directories) => Promise.all(directories.map(writeIndex)) )
+  writeImagesTable('logos'),
+  glob('banners/*').then( (directories) => Promise.all(directories.map(writeImagesTable)) ),
+  writeIndex(),
 ])
